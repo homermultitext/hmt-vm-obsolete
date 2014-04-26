8903391b40c1a3b27e6d5f8253a7a0b2aec13131
Neel Smith <dnsmith.neel@gmail.com>
2014-04-26 17:56:10 -0400

    Add initial Vagrant file and bootstrap shell script.

---
+++ Vagrantfile
@@ -0,0 +1,7 @@
+
+
+Vagrant.configure("2") do |config|
+  config.vm.box = 'saucy'
+  config.vm.box_url = 'http://cloud-images.ubuntu.com/vagrant/saucy/current/saucy-server-cloudimg-i386-vagrant-disk1.box'
+  config.vm.provision :shell, :path => "bootstrap.sh"
+end
---
+++ bootstrap.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Start from latest and greatest
+apt-get update
+
+# version control
+apt-get install -y git
+
+# need to have the one true editor
+# in case Neel Smith needs to work on
+# the machine
+apt-get install -y emacs
+
+# Get full JDK *now* so we don't pull in
+# jre as a dependency and have to
+# add jdk later...
+apt-get install -y openjdk-7-jdk
+
+# build system and dependency mgt
+apt-get install -y gradle
+apt-get install -y maven
+
+
+# tools for markdown: pandoc
+apt-get install -y pandoc
+
+# tools for markdown: beautifuldocs
+apt-get install -y nodejs
+if [ -h "/usr/bin/node" ]; then
+    echo "Already have symln for node"
+else
+    sudo ln -s /usr/bin/nodejs /usr/bin/node
+fi
+
+apt-get install -y npm
+npm install beautiful-docs
+
+# tools for markdown: mdpress
+apt-get install -y ruby2.0 ruby2.0-dev build-essential libssl-dev zlib1g-dev ruby-switch
+ruby-switch --set ruby2.0
+gem install mdpress
+
+# for building morpheus:
+apt-get flex-old
+
+# Update and trust apt to clean up some space for us:
+apt-get upgrade
+apt-get -y autoremove
+
+echo "Ran autoremove."
+
+## Need to build morpheus
+## Need to check out and update git repos.
+
+
+# ADD THIS....
+#echo "Setting up account directory."
+#cp "/vagrant/dotprofile" "/home/vagrant/.profile"
+
+
