6aa442c60e497e3fbefb2bae3eb5a00615951686
Neel Smith <dnsmith.neel@gmail.com>
2014-06-07 17:05:35 -0400

    Improve script.

--- scripts/idCodePoints.groovy
+++ scripts/idCodePoints.groovy
@@ -1,68 +1,30 @@
-import java.io.*
-import java.util.*
-
-/*
-*/
-
 
 if (args.length != 1) {
-println "Usage:  groovy idChars.groovy <FILENAME>"
-System.exit(1)
+  println "Usage:  groovy idChars.groovy <FILENAME>"
+  System.exit(1)
 }
-def fName = args[0]
 
-StringBuffer strBuff = new StringBuffer()
 
+File f = new File(args[0])
+StringBuffer strBuff
 try { 
-FileInputStream f = new FileInputStream(fName)
-InputStreamReader isr = new InputStreamReader(f,"UTF8")
-Reader inputReader = new BufferedReader(isr)
-int ch
-while ((ch = inputReader.read()) > -1) {
-      strBuff.append((char) ch);
-}
-inputReader.close()
-
+  strBuff   = new StringBuffer(f.getText("UTF-8"))
 } catch (Exception e) {
-println("Reading didn't work. "  + e.getMessage())
+  println("Couldn't get text of ${f}. "  + e.getMessage())
 }
 
-OutputStreamWriter out = new OutputStreamWriter(System.out, "UTF-8")
 
-// standard loop for cycling through code points in a StringBuffer:
+println "\nchar = dec. / hex."
+println "-------------------"
 int max = strBuff.codePointCount(0, strBuff.length() - 1)
-println "Number code points in input = " + max
+for (cpCount in 0..max) {
+  Integer codept = strBuff.codePointAt(cpCount)
+  String s =  new String(Character.toChars(codept))
 
-int idx = 0
-
-out.write "codept\thex\tstring\n"
-int codePoint = strBuff.codePointAt(idx)
-def s =  new String(Character.toChars(codePoint))
-out.write codePoint + "\t(" +Integer.toHexString(codePoint) +")\t" + s + "\n"
-
-while (idx < max) {
-      codePoint = strBuff.codePointAt(idx)
-  if (getFollowingCP(idx, strBuff) != null) {
-     int followingCodePoint = getFollowingCP(idx,strBuff)
-      s =  new String(Character.toChars(followingCodePoint))
-      out.write followingCodePoint + "\t(" +Integer.toHexString(followingCodePoint) +")\t" + s  
-      if (Character.isUpperCase(followingCodePoint)) {
-      out.write " (upper case)"
-      }
-      out.write "\n"
-      
+  if (codept == 10) {
+    println "<nl> = " + codept + " / " + Integer.toHexString(codept)
+  } else {
+    println s + "    = " + codept + " / " + Integer.toHexString(codept)
   }
-  idx = strBuff.offsetByCodePoints(idx,1)	
 }
 
-out.close()
-
-
-/*
-	peek ahead for next code point
-*/
-def getFollowingCP(int i, StringBuffer buff) {
-    int nextIdx = buff.offsetByCodePoints(i,1)	
-    int cp = buff.codePointAt(nextIdx)
-    return(cp)
-}
