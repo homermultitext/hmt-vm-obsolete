71967aadbc880481caf5193557a084aa479e9835
Neel Smith <dnsmith.neel@gmail.com>
2015-01-25 07:56:17 -0500

    Correct paths

--- system/initgui.sh
+++ system/initgui.sh
@@ -7,9 +7,10 @@ echo "Setting up account directory."
 # Configure menus:
 /bin/cp /vagrant/system/applications/*desktop /usr/share/applications 
 
-/bin/mdkir /home/vagrant/.local/share/applications
-/bin/mdkir /home/vagrant/.local/share/desktop-directories
+/bin/mkdir /home/vagrant/.local/share/applications
+/bin/mkdir /home/vagrant/.local/share/desktop-directories
+/bin/mkdir /home/vagrant/.config/menus
 
 /bin/cp /vagrant/system/user/applications/*desktop /home/vagrant/.local/share/applications 
-/bin/cp /vagrant/system/user/dt-directories/*desktop  /home/vagrant/.local/share/desktop-directories
+/bin/cp /vagrant/system/user/dt-directories/*directory  /home/vagrant/.local/share/desktop-directories
 cp /vagrant/system/user/menus/* /home/vagrant/.config/menus/lxde-applications.menu 
